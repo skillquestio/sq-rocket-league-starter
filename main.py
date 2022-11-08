8ea7f6981c991e95bf5f0052f4df68f0ef175b82
BuildTools <unconfigured@null.spigotmc.org>
2022-11-08 10:41:05 -0600

    end of logic lesson

--- main.py
+++ main.py
@@ -7,5 +7,18 @@ from util.routines import *
 class Bot(GoslingAgent):
     # This function runs every in-game tick (every time the game updates anything)
     def run(self):
-        # the line below tells the bot what it's trying to do
-        self.set_intent(jumper())
+        print('im running!')
+        if self.intent is not None:
+            return
+        d1 = abs(self.ball.location.y - self.foe_goal.location.y)
+        d2 = abs(self.me.location.y - self.foe_goal.location.y)
+        is_in_front_of_ball = d1 > d2
+        if self.kickoff_flag:
+            # the line below tells the bot what it's trying to do
+            self.set_intent(kickoff())
+            return
+        # if we're in front of the ball, retreat
+        if is_in_front_of_ball:
+            self.set_intent(goto(self.friend_goal.location))
+            return
+        self.set_intent(short_shot(self.foe_goal.location))
